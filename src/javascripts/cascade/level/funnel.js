209ab34d5b23a0ea374f4ff83ee2c5e693ba64bf
Ben Cole <cole@chapman.edu>
2015-04-01 16:40:01 -0700

    Implement funnel widget link show/hide in jQuery

--- src/javascripts/cascade/level/funnel.js
+++ src/javascripts/cascade/level/funnel.js
@@ -1,15 +1,40 @@
-$(function () {
+
+$( document ).ready(function() {
 
 	/* Funnel Toggle More Links
+	 * 
+	 * If the funnel boxes contain a list of links longer than 3 links, hide the rest and create a Show/Hide toggle
 	------------------------------------------------------------------------------------------------*/
-	$(".funnel .funnelBlock .moreLinks, .funnel .funnelBlock .more").toggle();
-	$(".funnel .funnelBlock .more").click(function () {
-	  $(this).prev(".moreLinks").slideToggle();
-	  if ($(this).html() === "+ More") {
-	    $(this).html("- Less");
-	  } else {
-	    $(this).html("+ More");
-	  }
+	$(".funnel-boxes-widget").each(function() {
+
+		// CONFIGURABLE:
+		var number_of_links_to_show = 3;
+
+		// Find the link list
+		var $list = $(this).find('.linkList');
+		var $extraLinks = $list.find('li').slice(number_of_links_to_show);
+
+		// Don't do anything if there aren't extra links
+		if ($list.length == 0 || $extraLinks.length == 0) return;
+
+		// Hide the extra links
+		$extraLinks.hide();
+
+		// Create the toggle button
+		$toggleButton = $('<a href="javascript:void(0);" class="more">+ More</a>');
+		$list.after($toggleButton);
+
+		// Bind action to show/hide on toggle button click
+		$toggleButton.on('click', function() {
+			if ($(this).html() === "+ More") {
+				$extraLinks.slideDown();
+				$(this).html("- Less");
+			} else {
+				$extraLinks.slideUp();
+				$(this).html("+ More");
+			}
+		});
+
 	});
 
 });
