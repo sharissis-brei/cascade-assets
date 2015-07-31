d7eb88faa106946d64f864b6603257f5b9d5fbf0
James Kerr <jkerr838@gmail.com>
2015-07-31 15:06:52 -0700

    Menu is almost ready with the responsiveness.

--- src/javascripts/master.js
+++ src/javascripts/master.js
@@ -1,62 +1,78 @@
-//= include cascade/lib/utils
+/*
+  = include objects/navigation
+*/
 
-//= include vendor/jquery.cycle.lite.min
-//= include vendor/jquery.hammer.min
-//= include vendor/modernizr-2.0.6.min
-//= include vendor/skrollr.min
 
-//= include cascade/plugins/amplify
-//= include cascade/plugins/cookie
-//= include cascade/plugins/ellipsis
-//= include cascade/plugins/flexslider
-//= include cascade/plugins/hover-intent
-//= include cascade/plugins/jcarousel
-//= include cascade/plugins/personalize
-//= include cascade/plugins/respond
-//= include cascade/plugins/super-pager
-//= include cascade/plugins/swf-object
-//= include cascade/plugins/touchwipe
-//= include cascade/level/plugins/tipsy
 
-//= include cascade/analytics
-//= include cascade/carousel
-//= include cascade/collapsed-nav
-//= include cascade/console-log
+
+
+
+
+
+
+ /* OLD STUFF BELOW HERE
+ ------------------------------------
+ */
+
+//  include cascade/lib/utils
+
+//  include vendor/jquery.cycle.lite.min
+//  include vendor/jquery.hammer.min
+//  include vendor/modernizr-2.0.6.min
+//  include vendor/skrollr.min
+
+//  include cascade/plugins/amplify
+//  include cascade/plugins/cookie
+//  include cascade/plugins/ellipsis
+//  include cascade/plugins/flexslider
+//  include cascade/plugins/hover-intent
+//  include cascade/plugins/jcarousel
+//  include cascade/plugins/personalize
+//  include cascade/plugins/respond
+//  include cascade/plugins/super-pager
+//  include cascade/plugins/swf-object
+//  include cascade/plugins/touchwipe
+//  include cascade/level/plugins/tipsy
+
+//  include cascade/analytics
+//  include cascade/carousel
+//  include cascade/collapsed-nav
+//  include cascade/console-log
 // don't include cascade/home
-//= include cascade/ie7
-//= include cascade/main
-//= include cascade/news-events
-//= include cascade/form-validation
+//  include cascade/ie7
+//  include cascade/main
+//  include cascade/news-events
+//  include cascade/form-validation
 
-//= include cascade/omni-nav.min
+//  include cascade/omni-nav.min
 
-//= include cascade/query-string
-//= include cascade/rotator
-//= include cascade/search
+//  include cascade/query-string
+//  include cascade/rotator
+//  include cascade/search
 // don't include cascade/shortcuts
-//= include cascade/video-overlay
-//= include cascade/weather
+//  include cascade/video-overlay
+//  include cascade/weather
 
 
 
-//= include cascade/level/accordion
-//= include cascade/level/degrees-programs
+//  include cascade/level/accordion
+//  include cascade/level/degrees-programs
 // don't include cascade/level/directory
-//= include cascade/level/faculty
-//= include cascade/level/feature
-//= include cascade/level/flickr-picasa
-//= include cascade/level/funnel
-//= include cascade/level/left-nav
-//= include cascade/level/level
-//= include cascade/level/masthead
-//= include cascade/level/mosaic
-//= include cascade/level/name-bar
-//= include cascade/level/news-events
-//= include cascade/level/placeholder
-//= include cascade/level/query-string
-//= include cascade/level/rotator
-//= include cascade/level/scroll-bar
-//= include cascade/level/social
-//= include cascade/level/sponsor
+//  include cascade/level/faculty
+//  include cascade/level/feature
+//  include cascade/level/flickr-picasa
+//  include cascade/level/funnel
+//  include cascade/level/left-nav
+//  include cascade/level/level
+//  include cascade/level/masthead
+//  include cascade/level/mosaic
+//  include cascade/level/name-bar
+//  include cascade/level/news-events
+//  include cascade/level/placeholder
+//  include cascade/level/query-string
+//  include cascade/level/rotator
+//  include cascade/level/scroll-bar
+//  include cascade/level/social
+//  include cascade/level/sponsor
 
-//= include objects/navigation
+//  include objects/navigation
--- src/javascripts/objects/navigation.js
+++ src/javascripts/objects/navigation.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
+  var $menu_toggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
 
-  menuToggle.on('click', function(e) {
+  $menu_toggle.on('click', function(e) {
     e.preventDefault();
     $('#js-navigation-menu').slideToggle(function(){
       if($('#js-navigation-menu').is(':hidden')) {
         $('#js-navigation-menu').removeAttr('style');
       }
     });
+  });
+
+  $('li.nav-link.more').on('click', function(e) {
+    if ($('#js-mobile-menu:visible').length) {
+      // This is the mobile view
+      e.preventDefault();
+      $('li.nav-link.more.active').removeClass('active');
+      $(this).addClass('active');
+    }
   });
 });
