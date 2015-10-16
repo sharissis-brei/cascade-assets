d6cf3bd32815f32df8cb40fd769823f4e8e23daa
James Kerr <jkerr838@gmail.com>
2015-10-16 16:37:46 -0700

    Fixed javascript height calculation.

--- src/javascripts/widgets/messaging_2_column.js
+++ src/javascripts/widgets/messaging_2_column.js
@@ -5,17 +5,19 @@ Messaging2Column.resize_timer =  null;
 Messaging2Column.centerMessagingMedia = function() {
   clearTimeout(Messaging2Column.resize_timer);
   Messaging2Column.resize_timer = setTimeout(function(){
-    var $text  = $('.messaging-2-column .text-column');
-    var $media = $('.messaging-2-column .media-column');
-    if (Media.smallScreen()) {
-      $media.css('height', 'auto');
-    } else {
-      $media.css('height', $text.height());
-    }
+    $('.messaging-2-column').each(function() {
+      var $text  = $(this).find('.text-column');
+      var $media = $(this).find('.media-column');
+      if (Media.smallScreen()) {
+        $media.css('height', 'auto');
+      } else {
+        $media.css('height', $text.height());
+      }
+    });
   }, 10);
 };
 
 
 // Initialize
-$(document).on('ready', function() {Messaging2Column.centerMessagingMedia(); });
+$(document).ready(function() {Messaging2Column.centerMessagingMedia(); console.log("Fired centering function on ready"); });
 $(window).on('resize', Messaging2Column.centerMessagingMedia);
