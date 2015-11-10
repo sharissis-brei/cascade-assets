0fabe42d51494c52c842b4f3145ba457fbedc799
James Kerr <jkerr838@gmail.com>
2015-11-10 14:21:43 -0800

    Fixed the law school theme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,20 @@
 class HomeController < ApplicationController
   def index
-    @one_column_pages   = html_files_in 'one_column'
-    @two_column_pages   = html_files_in 'two_column'
-    @three_column_pages = html_files_in 'three_column'
+    @one_column_pages   = html_files_in '1_column'
+    @two_column_pages   = html_files_in '2_column'
+    @three_column_pages = html_files_in '3_column'
   end
 
   def one_column
-    render template: "one_column/#{params[:page]}", layout: 'one_column'
+    render template: "1_column/#{params[:page]}", layout: '1_column'
   end
 
   def two_column
-    render template: "two_column/#{params[:page]}", layout: 'two_column'
+    render template: "2_column/#{params[:page]}", layout: '2_column'
   end
 
   def three_column
-    render template: "three_column/#{params[:page]}", layout: 'three_column'
+    render template: "3_column/#{params[:page]}", layout: '3_column'
   end
 
   private
