4fccdd02ad7925373d49e66789aa17579a05d6a8
Mirabel Rice <rice122@mail.chapman.edu>
2016-02-18 11:32:46 -0800

    added id to correct program select

--- app/assets/javascripts/widgets/hero_panel.js
+++ app/assets/javascripts/widgets/hero_panel.js
@@ -1,7 +1,12 @@
+/*
+* Cascade can't check managed links in select options, so this code gets the link from a hidden field when a program option is selected
+* http://www.hannonhill.com/kb/Linking/ 
+*/
+
 $(document).ready(function(){
-  var programNav = $('.program-select');
+  var programNav = $('#program-select-links');
   var links = programNav.children('input');
-  var programSelector = programNav.children('#program-select-links');
+  var programSelector = programNav.children('select');
   
   programSelector.on('change', function(){
   	var index = $(this).prop('selectedIndex');
