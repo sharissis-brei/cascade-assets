001dd6f73251fa0bcd7a5751ba202b878475634c
Mandy Thomas <mthomas@chapman.edu>
2016-04-26 23:24:41 -0700

    remove all faculty js from master.js
    don't require directory, faculty and new faculty-law (for now)

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -39,9 +39,9 @@
 
 //= require cascade/level/accordion
 //= require cascade/level/degrees-programs
-//= require cascade/level/directory
-//= require cascade/level/faculty
-//= require cascade/level/faculty-law
+// don't require cascade/level/directory
+// don't require cascade/level/faculty
+// don't require cascade/level/faculty-law
 //= require cascade/level/feature
 //= require cascade/level/flickr-picasa
 //= require cascade/level/funnel
