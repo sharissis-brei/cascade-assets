6c1c5ac0a3ac917b2fa75e907cc2dab9a8d5e718
Tom Atwell <atwell@chapman.edu>
2016-11-18 10:00:52 -0800

    Fixes DataDefinition bug that was breaking tests.

--- app/data_definitions/base.rb
+++ app/data_definitions/base.rb
@@ -7,7 +7,8 @@ module DataDefinitions
       xml_file = format('%s.xml', @name)
       xml_path = File.join(Rails.root, 'app/data_definitions/from_cascade', xml_file)
       @document = Nokogiri::XML(File.read(xml_path)) if File.exist?(xml_path)
-      @data_document = Nokogiri::XML(self.class::CASCADE_DATA.to_xml) if self.class::CASCADE_DATA
+      @data_document = Nokogiri::XML(self.class::CASCADE_DATA.to_xml) \
+        if self.class.const_defined?('CASCADE_DATA')
     end
 
     def set_defaults
