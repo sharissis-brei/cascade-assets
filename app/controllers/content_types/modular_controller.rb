246f25203a05b987fc6cd87139fd91ae13b16a34
khafa100 <khafa100@mail.chapman.edu>
2017-02-17 11:38:12 -0800

    Fixed alignment of parameters on lines 271 and 272 for rubocop offense.

--- app/controllers/content_types/modular_controller.rb
+++ app/controllers/content_types/modular_controller.rb
@@ -268,8 +268,8 @@ HTML
     def render_static_two_column_left_column
       # This reproduces content from the static sample version
       format("%s %s",
-        render_static_partial('widgets/left_column/callout_1'),
-        render_static_partial('widgets/left_column/callout_2'))
+             render_static_partial('widgets/left_column/callout_1'),
+             render_static_partial('widgets/left_column/callout_2'))
     end
 
     def render_static_three_column_primary_content
