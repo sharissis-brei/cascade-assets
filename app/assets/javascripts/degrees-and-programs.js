4308669455fff70f75c59fc2b9e72dea9666f1f7
Sarah Harissis <sharissis@barkleyrei.com>
2017-03-02 13:10:48 -0500

    Removed global JS includes

--- app/assets/javascripts/degrees-and-programs.js
+++ app/assets/javascripts/degrees-and-programs.js
@@ -1,6 +1,3 @@
-//= require_tree ../../../vendor/assets/javascripts
-//= require_tree ../../../lib/assets/javascripts
-
 //= require cascade/degrees-and-programs/foundation
 //= require cascade/degrees-and-programs/jquery.dotdotdot.min
 //= require cascade/degrees-and-programs/customSelect
