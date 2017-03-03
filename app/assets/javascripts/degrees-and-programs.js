22b8a0e5db3ce1ef927a5678d20d38f21d21c814
Sarah Harissis <sharissis@barkleyrei.com>
2017-03-03 15:31:29 -0500

    Removed degrees and programs assets from app folder

--- app/assets/javascripts/degrees-and-programs.js
+++
@@ -1,5 +0,0 @@
-//= require cascade/degrees-and-programs/foundation
-//= require cascade/degrees-and-programs/jquery.dotdotdot.min
-//= require cascade/degrees-and-programs/customSelect
-//= require cascade/degrees-and-programs/degreesAndPrograms
-//= require cascade/degrees-and-programs/degreesAndProgramsApp
