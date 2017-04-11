d92efec5f278bcf99947b2897cd03ca3670cd9eb
mandythomas <MandySThomas@hotmail.com>
2017-04-10 17:41:19 -0700

    chg up/down icons to svg not font; apply to themes
    switch the hide more/less arrows from being an icomoon font to using SVG
    path; edit all theme scss files to allow their color to be used for hide
    more/less button and arrows

--- app/assets/javascripts/cascade/level/shared-content-dept-contacts.js
+++ app/assets/javascripts/cascade/level/shared-content-dept-contacts.js
@@ -1,9 +1,9 @@
 $(function () {
 	/* button in Left Column Callout area's Contact Widget. Used to hide/show additional contact info */
-	$(".view-more-button").click(function () {
-		//Note toggleClass removes if class exists on element and adds if missing so can list both and will toggle between show and hide:		
-		$(this).siblings(".contact-widget").toggleClass("show-more").children(".more-content").slideToggle('slow');			
-		$(this).children(".view-more").toggleClass("show-more hide-more").children(".view-more").slideToggle('fast');
-		$(this).children(".view-less").toggleClass("hide-more show-more").children(".view-less").slideToggle('fast');
+	$(".view-more-expander").click(function () {
+		//Note toggleClass removes if class exists on element and adds if missing so can list both active and hidden and will toggle one on and other off:		
+		$(this).siblings(".editableContent").toggleClass("active inactive").children(".more-content").slideToggle('slow');			
+		$(this).children(".view-more").toggleClass("active inactive").children(".view-more").slideToggle('fast');
+		$(this).children(".view-less").toggleClass("inactive active").children(".view-less").slideToggle('fast');
 	});	
 });	
