a1f9f2980f3fb3d4a4e551955e7fded0d28bc645
Kayla Ziegler <kziegler@chapman.edu>
2017-08-09 16:34:43 -0700

    removed extra arguments, moved OffCanvas.init call, optimized syncLinkWidths, added constant
    fixed some spacing

--- app/assets/javascripts/omni-nav-v2.js
+++ app/assets/javascripts/omni-nav-v2.js
@@ -1,12 +1,59 @@
+
+
+var OffCanvasNav = (function() {
+
+  // Module Vars
+  var off_canvas_links;
+  var resizeId;
+
+  // Module Functions
+  var initialize = function() {
+    off_canvas_links = $('#js-off-canvas-nav > ul > li > a');
+    syncLinkWidths();
+
+    $('#js-off-canvas-trigger, #js-close-off-canvas-nav, #js-off-canvas-overlay').on('click', function(event) {
+      event.preventDefault();
+      $('#js-off-canvas-nav-container').toggleClass('open');
+      $('#js-off-canvas-overlay').toggleClass('active');
+      $('body').toggleClass('no-scroll');
+    });
+
+    $('#js-off-canvas-nav-container .toggle').on('click', function() {
+      $(this).parent().toggleClass('open'); // Targets li
+      $(this).parent().find('ul').slideToggle();
+    });
+
+    $(window).on('resize', function() {
+      clearTimeout(resizeId);
+      resizeId = setTimeout(syncLinkWidths, 500);
+    });
+  };
+
+  var syncLinkWidths = function() {
+    var width = $('#js-off-canvas-nav > ul').width();
+    off_canvas_links.css('width', width);
+  };
+
+  return {
+    init: initialize
+  };
+})();
+
+
 var OmniNav2 = (function() {
 
+  // Constants
+  var TABLET_BREAKPOINT = 768; //px
 
+  // Module Vars
   var $container, $utility_nav, $primary_nav;
 
-  var initialize = function(container, utility_nav, primary_nav) {
+  // Module Functions
+  var initialize = function(container) {
+    OffCanvasNav.init();
     $container = container;
-    $utility_nav = utility_nav;
-    $primary_nav = primary_nav;
+    $utility_nav = $container.find('.utility-nav');
+    $primary_nav = $container.find('#primary-nav');
     $('.utility-nav-trigger').on('click', onUtilityNavClick);
     $utility_nav.find('li.utility-has-dropdown').on('click', onUtilityNavDropdownClick);
     $utility_nav.find('input:text').on('input', onSearchInput);
@@ -16,7 +63,7 @@ var OmniNav2 = (function() {
   var onUtilityNavClick = function() {
     $('.primary-nav-action').toggleClass("utility-open");
 
-    if($(window).width() >= 768 ){
+    if ($(window).width() >= TABLET_BREAKPOINT) {
       $utility_nav.toggleClass('utility-nav-open');
       $('html.omni-nav-v2').toggleClass('utility-nav-open');  
       $primary_nav.removeClass('search-open');    
@@ -30,7 +77,7 @@ var OmniNav2 = (function() {
 
     // jQuery < 3.0 doesn't support toggleClass for SVGs
     // toggle open iff one of the search bars is open
-    if(primary_nav_classes.contains("search-open") || $utility_nav.hasClass('utility-nav-open')) {
+    if (primary_nav_classes.contains("search-open") || $utility_nav.hasClass('utility-nav-open')) {
       $('.icon-open-search').attr("class", "icon-open-search hide");
       $('.icon-close-search').attr("class", "icon-close-search");
     } else {
@@ -66,47 +113,6 @@ var OmniNav2 = (function() {
   };
 })();
 
-var OffCanvasNav = (function() {
-
-  var off_canvas_links;
-
-  var initialize = function() {
-    off_canvas_links = $('#js-off-canvas-nav > ul > li > a');
-    syncLinkWidths();
-
-    $('#js-off-canvas-trigger, #js-close-off-canvas-nav, #js-off-canvas-overlay').on('click', function(event) {
-      event.preventDefault();
-      $('#js-off-canvas-nav-container').toggleClass('open');
-      $('#js-off-canvas-overlay').toggleClass('active');
-      $('body').toggleClass('no-scroll');
-    });
-
-    $('#js-off-canvas-nav-container .toggle').on('click', function() {
-      $(this).parent().toggleClass('open'); // Targets li
-      $(this).parent().find('ul').slideToggle();
-    });
-
-    $(window).on('resize', function() {
-      syncLinkWidths();
-    });
-  };
-
-  var syncLinkWidths = function() {
-    var width = $('#js-off-canvas-nav > ul').width();
-    off_canvas_links.css('width', width);
-  };
-
-  return {
-    init: initialize
-  };
-})();
-
-
 $(document).ready(function () {
-  var container = $('#omni-nav-v2');
-  var utility_nav = container.find('.utility-nav');
-  var primary_nav = container.find('#primary-nav');
-
-  OmniNav2.init(container, utility_nav, primary_nav);
-  OffCanvasNav.init();
+  OmniNav2.init($('#omni-nav-v2'));
 });
