bf85776d1428f05e2932101213b0ac281a5008b7
Kayla Ziegler <kziegler@chapman.edu>
2017-08-09 13:56:49 -0700

    rewrote javascript using module pattern

--- app/assets/javascripts/omni-nav-v2.js
+++ app/assets/javascripts/omni-nav-v2.js
@@ -1,72 +1,75 @@
-var omni_nav_v2 = {
-
-  initialize: function() {
-    this.$container = $('#omni-nav-v2');
-    this.$utility_nav = this.$container.find('.utility-nav');
-    this.$primary_nav = this.$container.find('#primary-nav');
+var OmniNav2 = (function() {
 
 
-    $('.utility-nav-trigger').on('click', omni_nav_v2.onUtilityNavClick);
-    this.$utility_nav.find('li.utility-has-dropdown').on('click', omni_nav_v2.onUtilityNavDropdownClick);
-    
-    // container for search changes depending on screen width
-    // desktop, tablet: utility_nav
-    // mobile: primary_nav
-    this.$utility_nav.find('input:text').on('input', omni_nav_v2.onSearchInput);
-    this.$primary_nav.find('input:text').on('input', omni_nav_v2.onSearchInput);
-  },
+  var $container, $utility_nav, $primary_nav;
 
-  onUtilityNavClick : function() {
+  var initialize = function(container, utility_nav, primary_nav) {
+    $container = container;
+    $utility_nav = utility_nav;
+    $primary_nav = primary_nav;
+    $('.utility-nav-trigger').on('click', onUtilityNavClick);
+    $utility_nav.find('li.utility-has-dropdown').on('click', onUtilityNavDropdownClick);
+    $utility_nav.find('input:text').on('input', onSearchInput);
+    $primary_nav.find('input:text').on('input', onSearchInput); 
+  }
+
+  var onUtilityNavClick = function() {
+    $('.primary-nav-action').toggleClass("utility-open");
 
     if($(window).width() >= 768 ){
-      omni_nav_v2.$utility_nav.toggleClass('utility-nav-open');
+      $utility_nav.toggleClass('utility-nav-open');
       $('html.omni-nav-v2').toggleClass('utility-nav-open');  
-      omni_nav_v2.$primary_nav.removeClass('search-open');    
+      $primary_nav.removeClass('search-open');    
     } else {
-      omni_nav_v2.$utility_nav.removeClass('utility-nav-open');
+      $utility_nav.removeClass('utility-nav-open');
       $('html.omni-nav-v2').removeClass('utility-nav-open'); 
-      omni_nav_v2.$primary_nav.toggleClass('search-open');
+      $primary_nav.toggleClass('search-open');
     }
 
     var primary_nav_classes = document.getElementById('primary-nav').classList;
-    
+
     // jQuery < 3.0 doesn't support toggleClass for SVGs
     // toggle open iff one of the search bars is open
-    if(primary_nav_classes.contains("search-open") || omni_nav_v2.$utility_nav.hasClass('utility-nav-open')) {
+    if(primary_nav_classes.contains("search-open") || $utility_nav.hasClass('utility-nav-open')) {
       $('.icon-open-search').attr("class", "icon-open-search hide");
       $('.icon-close-search').attr("class", "icon-close-search");
     } else {
       $('.icon-open-search').attr("class", "icon-open-search");
       $('.icon-close-search').attr("class", "icon-close-search hide");
     }
+  };
 
-  },
-
-  onUtilityNavDropdownClick : function(e) {
+  var onUtilityNavDropdownClick = function(e) {
     e.stopPropagation();
     $(this).siblings('.utility-has-dropdown').removeClass('dropdown-open');
     $(this).toggleClass('dropdown-open');
-    $(document).on("click", omni_nav_v2.onDocumentClick);
-  },
+    $(document).on("click", onDocumentClick);
+  };
 
-  onDocumentClick : function() {
+  var onDocumentClick = function() {
     $('li.utility-has-dropdown').removeClass('dropdown-open');
-    $(document).off("click", omni_nav_v2.onDocumentClick);
-  },
+    $(document).off("click", onDocumentClick);
+  };
 
-  onSearchInput : function() {
+  var onSearchInput = function() {
     $('.search-icon').addClass('hide');
     $(document).on("click", function() {
-      omni_nav_v2.$utility_nav.find('input:text').val("");
-      omni_nav_v2.$primary_nav.find('input:text').val("");
+      // resets search input box when user clicks outside
+      $utility_nav.find('input:text').val("");
+      $primary_nav.find('input:text').val("");
       $('.search-icon').removeClass('hide');
     });
-  }
-}
+  };
 
-var off_canvas_nav_v2 = {
-  initialize: function() {
-    off_canvas_nav_v2.syncLinkWidths();
+  return {
+    init: initialize
+  };
+})();
+
+var OffCanvasNav = (function() {
+
+  var initialize = function() {
+    syncLinkWidths();
 
     $('#js-off-canvas-trigger, #js-close-off-canvas-nav, #js-off-canvas-overlay').on('click', function(event) {
       event.preventDefault();
@@ -81,17 +84,26 @@ var off_canvas_nav_v2 = {
     });
 
     $(window).on('resize', function() {
-      off_canvas_nav_v2.syncLinkWidths();
+      syncLinkWidths();
     });
-  },
+  };
 
-  syncLinkWidths: function() {
+  var syncLinkWidths = function() {
     var width = $('#js-off-canvas-nav > ul').width();
     $('#js-off-canvas-nav > ul > li > a').css('width', width);
-  }
-}
+  };
+
+  return {
+    init: initialize
+  };
+})();
+
 
 $(document).ready(function () {
-  omni_nav_v2.initialize();
-  off_canvas_nav_v2.initialize();
+  var container = $('#omni-nav-v2');
+  var utility_nav = container.find('.utility-nav');
+  var primary_nav = container.find('#primary-nav');
+
+  OmniNav2.init(container, utility_nav, primary_nav);
+  OffCanvasNav.init();
 });
