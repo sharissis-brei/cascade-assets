2eaa7aa7856790063634c09bce3cbf3b44cbef33
Tom Atwell <atwell@chapman.edu>
2017-08-22 11:15:36 -0700

    Adds demo for branded masthead.

--- app/controllers/omninav_controller.rb
+++ app/controllers/omninav_controller.rb
@@ -5,7 +5,10 @@ class OmninavController < ApplicationController
     render html: omninav_html, layout: 'omninav'
   end
 
+  # GET /omni_nav/omni_nav_v2(/:context)
   def demo_v2
+    # params[:context] can be used to simulate different contexts (e.g. /omninav_v2/law)
+    @branding_class = params[:context].nil? ? 'unbranded' : 'branded'
     render template: "omni_nav/omni_nav_v2", layout: 'omninav'
   end
 end
