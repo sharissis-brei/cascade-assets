799e5b7b173b16880d6d42692126aabd09231ace
Tom Atwell <atwell@chapman.edu>
2017-08-23 14:30:03 -0700

    Sorts out markup and classing for off-canvas logo.

--- app/controllers/omninav_controller.rb
+++ app/controllers/omninav_controller.rb
@@ -8,8 +8,21 @@ class OmninavController < ApplicationController
   # GET /omni_nav/omni_nav_v2(/:context)
   def demo_v2
     # params[:context] can be used to simulate different contexts (e.g. /omninav_v2/law)
-    @branding_class = params[:context].nil? ? 'unbranded' : 'branded'
     @theme_class = params[:context]
+
+    # Apparently this is a thing in Cascade. Kill me. See for instance the law home pages
+    # where DOM tree goes:
+    # body > div#theme.law > ... > div.cu-off-canvas-header > a.sc-logo.fowler
+    url_theme_map = {
+      # theme class => logo class
+      'business' => 'asbe',
+      'law' => 'fowler'
+    }
+    @logo_class = url_theme_map.fetch(@theme_class, 'default-logo')
+
+    # I believe the innovation of this selector is new with OmniNav v2
+    @branding_class = params[:context].nil? ? 'unbranded' : 'branded'
+
     render template: "omni_nav_v2/omninav", layout: 'omninav'
   end
 end
