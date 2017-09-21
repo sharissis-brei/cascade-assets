cd8c7a7502db4749102167bdcb34d3235c573b17
Tom Atwell <atwell@chapman.edu>
2017-09-21 15:25:14 -0700

    Refactors omninav controller demo_v2 action to function more like Cascade with umbrella categories.

--- app/controllers/omninav_controller.rb
+++ app/controllers/omninav_controller.rb
@@ -8,27 +8,29 @@ class OmninavController < ApplicationController
   # GET /omni_nav/omni_nav_v2(/:context)
   def demo_v2
     # params[:context] can be used to simulate different contexts (e.g. /omninav_v2/law)
-    @theme_class = params[:context]
+    cascade_context = params[:context]
 
+    # Umbrella Categories: this is hardcoded in the OmniNav velocity format in Cascade
     # Apparently this is a thing in Cascade. Kill me. See for instance the law home pages
     # where DOM tree goes:
     # body > div#theme.law > ... > div.cu-off-canvas-header > a.sc-logo.fowler
-    theme_logo_map = {
-      # theme class => logo class
-      'business' => 'asbe',
-      'law' => 'fowler'
-    }
-    @logo_class = theme_logo_map.fetch(@theme_class, 'default-logo-cu')
+    umbrella_categories = [
+      # [ directory (assume leading /), menu label, class/image id, path, brand text ]
+      [ 'business', 'ARGYROS SCHOOL', 'asbe', 'business/index.aspx', 'Argyros School of Business' ],
+      [ 'education', 'ATTALLAH COLLEGE', 'education', 'education/index.aspx' ],
+      [ 'dodge', 'DODGE COLLEGE', 'dodge', 'dodge/index.aspx' ],
+      [ 'crean', 'CREAN COLLEGE', 'crean', 'crean/index.aspx' ],
+      [ 'wilkinson', 'WILKINSON COLLEGE', 'wilkinson', 'wilkinson/index.aspx' ],
+      [ 'copa', 'CoPA', 'copa', 'copa/index.aspx' ],
+      [ 'pharmacy', 'CUSP', 'cusp', 'pharmacy/index.aspx' ],
+      [ 'law', 'FOWLER SCHOOL OF LAW', 'fowler', 'law/index.aspx', 'Fowler School of Law' ]
+    ]
 
-    # I believe the identification of the following variables will have to be added
-    # to Cascade (in the velocity format?) for OmniNav v2.
-    theme_brand_map = {
-      # theme class => brand_text
-      'law' => 'Fowler School of Law'
-    }
-    @brand_text = theme_brand_map.fetch(@theme_class, '')
-
-    @brand_class = params[:context].nil? ? 'unbranded' : 'branded'
+    # Set umbrella category. This is more-or-less how the Cascade format does it
+    @page_umbrella_category = []
+    umbrella_categories.each do | umbrella_category |
+      @page_umbrella_category = umbrella_category if umbrella_category.first == cascade_context
+    end
 
     render template: "omni_nav_v2/omninav", layout: 'omninav'
   end
