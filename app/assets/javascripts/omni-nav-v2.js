cb0f7c998424d86ff8fa607e33981583e5fb3896
mirabelrice <rice122@mail.chapman.edu>
2017-10-11 16:18:36 -0700

    started implementing js functionality for search

--- app/assets/javascripts/omni-nav-v2.js
+++ app/assets/javascripts/omni-nav-v2.js
@@ -1,4 +1,3 @@
-
 var OmniNav2 = (function() {
 
   // Constants
@@ -11,14 +10,15 @@ var OmniNav2 = (function() {
 
   // Module Functions
   var initialize = function(container) {
+    GoogleCustomSearch.init();
     OffCanvasNav.init();
     $container = container;
     $utilityNav = $container.find('.utility-nav');
     $primaryNav = $container.find('#primary-nav');
+    $utilitySearch = $utilityNav.find('.utility-search');
     $('.utility-nav-trigger').on('click', onUtilityNavClick);
     $utilityNav.find('li.utility-has-dropdown').on('click', onUtilityNavDropdownClick);
-    $utilityNav.find('input:text').on('input', onSearchInput);
-    $primaryNav.find('input:text').on('input', onSearchInput);
+    $utilitySearch.find('.search-type-option').on('click', onUtilitySearchSelect);
 
     // Remove padding from theme version. Have to use js because css will not work.
     // See https://stackoverflow.com/a/1014958/6763239
@@ -72,6 +72,179 @@ var OmniNav2 = (function() {
       $('.search-icon').removeClass('hide');
     });
   };
+
+  var onUtilitySearchSelect = function() {
+    var searchType = $(this).children('a').text();
+    $("#selected-search-type").text(searchType);
+  };
+
+  var GoogleCustomSearch = (function() {
+    var GCS_ENGINE_ID = '015856566681218627934:2ndbiubovo4';
+    var GCS_SOURCE = location.protocol + '//www.google.com/cse/cse.js?cx=' + GCS_ENGINE_ID;
+    var searchElements = [];
+
+    var initialize = function() {
+      searchElements = $(".cu-search-box");
+
+      // Must define window.__gsce before GCS script loads
+      // Source: https://developers.google.com/custom-search/docs/tutorial/implementingsearchbox
+      window.__gcse = {
+        parsetags: 'explicit',
+        callback: loadGCSElements
+      };
+
+      // GCS is also loaded in omni-nav.js. Only load script if it doesnt exist already.
+      if($('script[src="'+ GCS_SOURCE +'"]').length == 0) {
+        (function(){
+          var cx = GCS_ENGINE_ID;
+          var gcse = document.createElement('script');
+          gcse.type = 'text/javascript';
+          gcse.async = true;
+          gcse.src = GCS_SOURCE;
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(gcse, s);
+        })();
+      }
+    };
+
+    var loadGCSElements = function() {
+      searchElements.each(function() {
+        var element = new TwoColumnGCS();
+        element.init($(this));
+      });
+    };
+
+    // two-column GCS(named by Google) layout consists of a search box and separate search results container
+    // Layout option is set in the GCS control panel
+    var TwoColumnGCS = function() {
+      var SEARCH_RESULTS_BASE_URL = "www.chapman.edu/search-results/index.aspx?";
+      var ENTER_KEY = 13;
+      var ESC_KEY = 27;
+
+      var $searchBox,
+          $searchResults,
+          $searchResultsContainer,
+          $loadMoreResultsButton,
+          elementName,
+          searchBoxConfig,
+          searchResultsConfig,
+          gcsElement;
+
+      var initialize = function($el) {
+        $searchBox = $el;
+        $searchResultsContainer = $searchBox.next('.search-results-container');
+        $searchResults = $searchResultsContainer.children('.cu-search-results');
+        elementName = $searchBox[0].id;
+
+        searchBoxConfig = {
+          gname: elementName,
+          div: $searchBox[0],
+          tag: 'searchbox',
+          attributes: {
+            enableAutoComplete: true,
+            autoCompleteMatchType: 'any',
+            resultSetSize: 6,
+            enableHistory: false
+          }
+        };
+
+        searchResultsConfig = {
+          gname: elementName,
+          div: $searchResults[0],
+          tag: 'searchresults',
+          attributes: {
+            linkTarget: '_self',
+            enableOrderBy: true
+          }
+        };
+
+        renderGCSMarkup();
+        updateSearchResultsHeight();
+
+        //elements created after GCS markup is created
+        gcsElement = google.search.cse.element.getElement(elementName);
+        $loadMoreResultsButton = $searchResults.find('.more-results');
+
+        $searchBox.find('input.gsc-search-button').on('click', onSearchEnter);
+        $searchBox.find('input.gsc-input').on('keyup', onSearchEnter);
+        $searchBox.find('.gsc-clear-button').on('click', hideSearchResults);
+      };
+
+      var onSearchEnter = function(e) {
+        if((e.type == 'click' || e.which == ENTER_KEY) && gcsElement.getInputQuery().length > 0){
+          showSearchResults();
+        }
+      }
+
+      var onSearchEsc = function(e) {
+        if(e.which == ESC_KEY){ hideSearchResults(); }
+      }
+
+      var onDocumentClick = function(e) {
+        if($searchResultsContainer.not(e.target) && $searchResultsContainer.has(e.target).length === 0
+          && $searchBox.not(e.target) && $searchBox.has(e.target).length === 0 ) {
+          hideSearchResults();
+        }
+      }
+
+      var renderGCSMarkup = function() {
+        google.search.cse.element.render(searchBoxConfig, searchResultsConfig);
+        $searchBox.find('input.gsc-input').attr('placeholder', 'Search');
+        $searchResults.find('.gsc-control-cse').append($('<a>', {class: "more-results", href: SEARCH_RESULTS_BASE_URL}));
+      };
+
+      var showSearchResults = function() {
+        var term = gcsElement.getInputQuery();
+        $loadMoreResultsButton.text('See more results for "'+term+'"');
+        $searchResultsContainer.fadeIn(200);
+        lockScroll();
+
+        $(document).on('keyup', onSearchEsc);
+        $(document).on('click', onDocumentClick);
+      };
+
+      var hideSearchResults = function() {
+        $searchResultsContainer.fadeOut(200);
+        unlockScroll();
+
+        $(document).off('keyup', onSearchEsc);
+        $(document).off('click', onDocumentClick);
+      };
+
+      var updateSearchResultsHeight = function() {
+        $searchResultsContainer.height($(window).height());
+      };
+
+      var lockScroll = function() {
+        // lock scroll position, but retain settings for later
+        var scrollPosition = [
+          self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+          self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+
+        var html = $('html');
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+      };
+
+      var unlockScroll = function() {
+        var html = $('html');
+        var scrollPosition = html.data('scroll-position');
+        html.css('overflow', 'visible');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+      };
+
+      return {
+        init: initialize,
+      };
+    };
+
+    return {
+      init: initialize
+    };
+  })();
 
   // This module handles functionality associated with OffCanvasNav in particular.
   var OffCanvasNav = (function() {
