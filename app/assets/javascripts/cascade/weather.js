43a32083f1c811083bd7506dc07318d925d91d1d
mandythomas <MandySThomas@hotmail.com>
2018-03-09 15:05:47 -0800

    fix formatting of recently edited file
    use 2-space tabs and remove blank lines; add in a comment to help
    understand purpose of code

--- app/assets/javascripts/cascade/weather.js
+++ app/assets/javascripts/cascade/weather.js
@@ -1,19 +1,16 @@
 $(function () {
-
-	/* Populate weather from json feed; default on page is 65 until modified by this routine
-    ------------------------------------------------------------------------------------------------*/
-	//sample data: {"weather":{"icon_path":"\/images\/icons\/weather\/2cloud_norain.png","temp_f":"66","temp_c":"19"}}
-	$.getJSON("https://forecast.chapman.edu/chapman/banner-json.php?callback=orange", function (data) {	
-
-        var iconPath = data.weather.icon_path,
-            tempF = data.weather.temp_f,
-            tempC = data.weather.temp_c,
-            url = "//www.chapman.edu",
-            $weather = $(".weather");
-
-        $weather.find("img").attr("src", url + iconPath);
-        $weather.find(".temp .f").html(tempF + "&deg; F");
-        $weather.find(".temp .c").html(tempC + "&deg; C");
-    });
-
+  /* Populate weather from json feed; default on page is 65 until modified by this routine
+  ------------------------------------------------------------------------------------------------*/
+  //sample data: {"weather":{"icon_path":"\/images\/icons\/weather\/2cloud_norain.png","temp_f":"66","temp_c":"19"}}
+  $.getJSON("https://forecast.chapman.edu/chapman/banner-json.php?callback=orange", function (data) {	
+    var iconPath = data.weather.icon_path,
+      tempF = data.weather.temp_f,
+      tempC = data.weather.temp_c,
+      url = "//www.chapman.edu",
+      $weather = $(".weather");
+    //find weather class in html and substitute placeholders with current temp
+    $weather.find("img").attr("src", url + iconPath);
+    $weather.find(".temp .f").html(tempF + "&deg; F");
+    $weather.find(".temp .c").html(tempC + "&deg; C");
+  });
 });
