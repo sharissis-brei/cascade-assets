2251ede4f4594777da4f61d5ad0af015132f8935
Kayla Ziegler <kziegler@chapman.edu>
2018-03-30 14:29:04 -0700

    Updates emergency alert description from Rave feed

--- app/assets/javascripts/widgets/emergency-alert.js
+++ app/assets/javascripts/widgets/emergency-alert.js
@@ -5,4 +5,12 @@ $(document).ready( function() {
   trigger.on('click', function() {
     emergencyAlertDiv.slideUp("slow");
   });
+
+  $.get('https://content.getrave.com/rss/chapman/channel1', function (data) {
+    $(data).find("item").each(function () {
+      var item = $(this);
+      var description = item.find("description").text();
+      emergencyAlertDiv.find("p.alert-message").html(description);
+    });
+  });
 });
