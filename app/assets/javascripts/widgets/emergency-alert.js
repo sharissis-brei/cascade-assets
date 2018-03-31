8efee86d76b25189017cf8b9c59df58c4dc5a0ce
Kayla Ziegler <kziegler@chapman.edu>
2018-03-30 17:47:01 -0700

    Javascript for displaying rave emergency alerts

--- app/assets/javascripts/widgets/emergency-alert.js
+++ app/assets/javascripts/widgets/emergency-alert.js
@@ -1,16 +1,50 @@
 $(document).ready( function() {
-  var trigger = $('.close-alert-trigger');
   var emergencyAlertDiv = $('div.emergency-alert');
+  var alertMessage = $('p.alert-message')[0];
+  var raveFeed = 'https://content.getrave.com/rss/chapman/channel1';
 
-  trigger.on('click', function() {
-    emergencyAlertDiv.slideUp("slow");
-  });
+  var displayEmergencyAlert = function() {
+    emergencyAlertDiv.show();
+    var trigger = $('.close-alert-trigger');
+    addCloseTrigger(trigger);
+  };
 
-  $.get('https://content.getrave.com/rss/chapman/channel1', function (data) {
-    $(data).find("item").each(function () {
-      var item = $(this);
-      var description = item.find("description").text();
-      emergencyAlertDiv.find("p.alert-message").html(description);
+  var addCloseTrigger = function(trigger) {
+    trigger.on('click', function() {
+      emergencyAlertDiv.slideUp("slow");
     });
-  });
+  };
+
+  // Looks for key phrase in Rave feed to indicate no emergency
+  var checkEmergencyFeed = function() {
+    $.get(raveFeed, function (data) {
+      $(data).find("item").each(function () {
+        return $(this).find("title").text().includes("There is no emergency");
+      });
+    });
+  };
+
+  var getRaveFeedData = function() {
+    $.get(raveFeed, function (data) {
+      // There should really only be 1 item in the feed at any given time
+      $(data).find("item").each(function () {
+        var item = $(this);
+        var raveFeedDescription = item.find("description").text();
+        emergencyAlertDiv.find('p.alert-message').html(raveFeedDescription);
+        var raveFeedTitle = item.find("title").text();
+        emergencyAlertDiv.find('p.alert-title').html(raveFeedTitle);
+      });
+    });
+  };
+
+  // If there is already a message (comes from data def in Cascade),
+  // don't override the HTML with Rave feed
+  var isEmergency = checkEmergencyFeed();
+  if (alertMessage.innerHTML != '') {
+    displayEmergencyAlert();
+  }
+  else if(isEmergency) {
+    displayEmergencyAlert();
+    getRaveFeedData();
+  }
 });
